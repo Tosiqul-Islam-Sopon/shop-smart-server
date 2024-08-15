91afdd2cf655e7218bbd9e8bf346a9f72dc94df0
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-08-16 01:24:51 +0600

    pagination appiled in beck end

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,9 +9,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nnvexxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,15 +22,35 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+        // Connect the client to the server (optional starting in v4.7)
         await client.connect();
         const database = client.db("shop-smart");
         const productCollection = database.collection("products");
 
         app.get("/products", async (req, res) => {
-            const products = await productCollection.find().toArray();
-            res.send(products);
-        })
+            const page = parseInt(req.query.page) || 1;
+            const limit = parseInt(req.query.limit); 
+            const skip = (page - 1) * limit;
+
+            try {
+                const products = await productCollection.find().skip(skip).limit(limit).toArray();
+                const totalProducts = await productCollection.countDocuments();
+                const totalPages = Math.ceil(totalProducts / limit);
+
+                res.json({
+                    products,
+                    pagination: {
+                        currentPage: page,
+                        totalPages,
+                        totalProducts,
+                        perPage: limit
+                    }
+                });
+            } catch (error) {
+                res.status(500).json({ message: error.message });
+            }
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -43,7 +60,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
